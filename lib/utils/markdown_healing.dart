8740ba240115114d4834ad528f3f057c6177b8c3
Seungju08 <cntmdwn5522@gmail.com>
2026-06-17 17:06:28 +0900

    feat: 스트리밍 마크다운 보정 헬퍼 healStreamingMarkdown 추가

---
+++ lib/utils/markdown_healing.dart
@@ -0,0 +1,23 @@
+/// 스트리밍 중인 마크다운을 렌더링하기 전 보정한다.
+///
+/// 누적된 원본 [source]는 변경하지 않고, 아직 닫히지 않은 코드펜스(```),
+/// 인라인 코드(`), 굵게(**)에 임시로 닫는 짝을 붙인 새 문자열을 반환한다.
+/// 코드펜스 안쪽 텍스트는 인라인 보정 대상에서 제외된다.
+String healStreamingMarkdown(String source) {
+  final fenceCount = RegExp(
+    r'^```',
+    multiLine: true,
+  ).allMatches(source).length;
+
+  var healed = fenceCount.isOdd ? '$source\n```' : source;
+
+  final outsideFences = healed.replaceAll(RegExp(r'```[\s\S]*?```'), '');
+
+  final backtickCount = outsideFences.split('`').length - 1;
+  if (backtickCount.isOdd) healed = '$healed`';
+
+  final boldCount = RegExp(r'\*\*').allMatches(outsideFences).length;
+  if (boldCount.isOdd) healed = '$healed**';
+
+  return healed;
+}
